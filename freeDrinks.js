3f1958820855028487e409f77f3a5b024bb76646
Ariful <arifulq234@gmail.com>
2025-07-28 01:48:50 +0600

    first commit

---
+++ freeDrinks.js
@@ -0,0 +1,38 @@
+// const burger=400;
+// const coke=30;
+// const iswansdrinks=true;
+// if(burger>=500){
+//     console.log("Coke is free")
+// }
+// else if (!iswansdrinks) {
+//   console.log("Totalcost:", burger + coke);
+// } else {
+//   console.log("Cost Is:", burger);
+// }
+
+
+
+// ******another types
+
+// const burger=400;
+// const coke=30;
+// const iswansdrinks=true;
+// if(burger>=500){
+//     console.log("Coke is free")
+// }
+// else if (!iswansdrinks) {
+//   console.log("Totalcost:", burger + coke);
+// } else {
+//   console.log("Cost Is:", burger);
+// }
+const burger=400;
+const coke=30;
+const iswansdrinks=false;
+if(burger>=500){
+    console.log("Coke is free")
+}
+else if (iswansdrinks) {
+  console.log("Totalcost:", burger + coke);
+} else {
+  console.log("Cost Is:", burger);
+}
