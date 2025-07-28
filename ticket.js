6839448cb5e5300680d9257e81689b205764807a
Ariful <arifulq234@gmail.com>
2025-07-29 01:49:12 +0600

    Exploring bus ticket fare

--- ticket.js
+++ ticket.js
@@ -4,3 +4,17 @@
 //     - Senior citizens (age >= 60) gets a 15% Discount
 //     - Otherwise Regular ticket fare 800 tk
 
+var age=60;
+// var isstudent=true;
+var isstudent=false;
+
+if(!isstudent){
+    console.log("Fare is 50%")
+}
+else if(age<10){
+    console.log("you can travel free")
+}
+else if(age>=60){
+    console.log("your Have recived 15% Discount")
+}
+else(console.log("Your have to pay regular fare"))
