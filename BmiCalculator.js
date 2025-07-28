841f4d5e92f1724ce07c217e4418379672cd0dd6
Ariful <arifulq234@gmail.com>
2025-07-28 15:09:15 +0600

    EXpolring BMI Calculator

---
+++ BmiCalculator.js
@@ -0,0 +1,21 @@
+// const weight = 68;
+// // height in meter
+// const height = 1.651;
+// let BMI = weight / (height * height);
+// if(BMI<18.5){
+//     console.log("your are under weight")
+// }
+// else if(BMI>=18.5 && BMI<=24.9){
+//     console.log('your are normal')
+// }
+// else if ( BMI>24.9 && BMI<=29.9){
+//      console.log("you are overweight")
+//     }
+// else {console.log("Your are obese.")}
+
+
+const weight = 68;
+// // height in meter
+const height = 1.651;
+let BMI = weight / (height * height);
+// Here the BMI
